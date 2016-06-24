0fb323994443100bab7bf1dbc19f7c18a0948cfa
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-06-24 13:38:56 -0500

    added menu and animation mixins

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 // put your code here
 // remember, you can use modules with browserify
+$(function() {
+  
+
+  var windowHeight = window.innerHeight;
+  var scrollArea = 1000 - windowHeight;
+  var nav = $('nav');
+
+  $(window).on('scroll', function() {
+    var scrollTop = window.pageYOffset || window.scrollTop;
+    var scrollPercent = scrollTop/scrollArea || 0;
+
+    nav.css('margin-top', '-' + (scrollPercent*window.innerHeight/6) + 'px');
+  });
+  
+});
