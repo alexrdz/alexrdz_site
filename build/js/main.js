8db3b3f5c20a60c4d2406138ac47ae664aa6bc5a
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-06-25 12:47:33 -0500

    initial build subtree commit

---
+++ build/js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+  
+
+  // Desktop Navigation
+  /*----------------------------------------------------------------*/  
+  var windowHeight = window.innerHeight;
+  var scrollArea = 1000 - windowHeight;
+  var nav = $('nav');
+
+  if(windowHeight > 600) {
+    $(window).on('scroll', function() {
+      var scrollTop = window.pageYOffset || window.scrollTop;
+      var scrollPercent = scrollTop/scrollArea || 0;
+
+      nav.css('margin-top', '-' + (scrollPercent*window.innerHeight/8) + 'px');
+    });
+  }
+
+
+  // Responsive Navigation
+  /*----------------------------------------------------------------*/  
+  var $body = $('body'),
+      $trigger = $('.js-nav-trigger');
+
+  $trigger.on('click', function() {
+    ($trigger, $body).toggleClass('is-active'); 
+  });
+
+  
+});
