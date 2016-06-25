724c6352ab5a565cf72c7193b883ab2c7eae6233
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-06-24 19:43:22 -0500

    mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,30 @@
-// put your code here
-// remember, you can use modules with browserify
 $(function() {
   
 
+  // Desktop Navigation
+  /*----------------------------------------------------------------*/  
   var windowHeight = window.innerHeight;
   var scrollArea = 1000 - windowHeight;
   var nav = $('nav');
 
-  $(window).on('scroll', function() {
-    var scrollTop = window.pageYOffset || window.scrollTop;
-    var scrollPercent = scrollTop/scrollArea || 0;
+  if(windowHeight > 600) {
+    $(window).on('scroll', function() {
+      var scrollTop = window.pageYOffset || window.scrollTop;
+      var scrollPercent = scrollTop/scrollArea || 0;
 
-    nav.css('margin-top', '-' + (scrollPercent*window.innerHeight/6) + 'px');
+      nav.css('margin-top', '-' + (scrollPercent*window.innerHeight/8) + 'px');
+    });
+  }
+
+
+  // Responsive Navigation
+  /*----------------------------------------------------------------*/  
+  var $body = $('body'),
+      $trigger = $('.js-nav-trigger');
+
+  $trigger.on('click', function() {
+    ($trigger, $body).toggleClass('is-active'); 
   });
+
   
 });
